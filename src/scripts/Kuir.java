33ad7a6fa8ffc7c52dac2a6e3bdec812cc35a5de
aesa117 <handong227@gmail.com>
2022-03-30 23:03:59 +0900

    최종 수정

--- src/scripts/Kuir.java
+++ src/scripts/Kuir.java
@@ -12,19 +12,17 @@ public class Kuir {
 	public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, SAXException, ClassNotFoundException {
 		String command = args[0];   
 		String path = args[1];
-		String path2 = args[2];
-		String path3 = args[3];
 
 		if(command.equals("-c")) {
 			MakeCollection collection = new MakeCollection(path);
 			collection.makeXml();
 		}
 		else if(command.equals("-k")) {
-			MakeKeyword keyword = new MakeKeyword(path2);
+			MakeKeyword keyword = new MakeKeyword(path);
 			keyword.convertXml();
 		}
 		else if(command.equals("-i")) {
-			Indexer indexer = new Indexer(path3);
+			Indexer indexer = new Indexer(path);
 			indexer.makeIndexer();
 		}
 
